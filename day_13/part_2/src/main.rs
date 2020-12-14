b233771627aa219587d913b474870ef3738944de
ajgoodm <aarongoodm@gmail.com>
2020-12-13 20:40:40 -0500

    comments

--- day_13/part_2/src/main.rs
+++ day_13/part_2/src/main.rs
@@ -23,6 +23,15 @@ fn find_departure_time(periods_remainders: Vec<(i64, i64)>) {
     let (mut running_product, _): (i64, i64) = *pr_iter.next().unwrap();
     let mut running_sum: i64 = 0;
 
+    // There some funky stuff happening here. We iteratively find
+    // a time that works for each remainder. _A_ time t_i which
+    // equals r_i mod p_i for each remainder. When we move to the
+    // next bus, we want to find a time t_i+1 that satisfies all
+    // previous buses as well. We need to take the previous time
+    // and add the least common multiple (LCM) of the previous buses
+    // periods, because this will be the smallest number that is
+    // 0 mod all the previous ps. Our buses all have priume periods,
+    // so the LCM of any collection of buses is their product.
     for (period, remainder) in pr_iter {
         while running_sum % period != *remainder {
             running_sum += running_product;
@@ -52,7 +61,16 @@ fn parse_input(mut lines: Box<dyn Iterator<Item = String>>) -> Vec<(i64, i64)> {
         })
         .map( |(idx, capture)| {
             let period = capture.as_str().parse::<i64>().unwrap();
+            // The lore says the index in this list is how many
+            // minutes after our special time t_0, the bus with
+            // this period will need to arrive. That means that
+            // the special time t_0 occurs idx  units _before_
+            // a time which is 0 mod p (the period of the bus)
             let mut idx = period - idx as i64;
+
+            // we only care about the "remainder" modulo the
+            // period (the bus ID), so we can +/- period with
+            // impunity.
             while idx < 0{idx += period}
             (period, idx)
         }).collect()
@@ -82,13 +100,4 @@ impl Iterator for AocBufReader {
             None => None
         }
     }
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-
-
 }
