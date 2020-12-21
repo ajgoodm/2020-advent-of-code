0ac2560e9ba010fa7298ba42f63ae6f9b5332d3d
ajgoodm <aarongoodm@gmail.com>
2020-12-20 19:02:52 -0500

    day 17, huzzah!

--- day_17/part_2/src/main.rs
+++ day_17/part_2/src/main.rs
@@ -4,9 +4,8 @@ use std::io::{BufRead, BufReader, Lines};
 
 use itertools::Itertools;
 
-const ARRAY_EDGE: usize = 48;
-const HALF_ARRAY_EDGE: usize = 24;
-const SHIFT: usize = 24;
+const ARRAY_EDGE: usize = 28;
+const HALF_ARRAY_EDGE: usize = 14;
 
 
 fn main() {
@@ -23,12 +22,13 @@ fn main() {
 
 }
 
-fn make_cube(boxed_iterator: Box<dyn Iterator< Item = String >>) -> [[[bool; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE] {
-    let mut cube = [[[false; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE];
+fn make_cube(boxed_iterator: Box<dyn Iterator< Item = String >>)
+    -> [[[[bool; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE] {
+    let mut cube = [[[[false; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE];
     for (y_idx, line) in boxed_iterator.enumerate() {
         for (x_idx, c) in line.chars().enumerate() {
             match c {
-                '#' => cube[0 + HALF_ARRAY_EDGE][y_idx + HALF_ARRAY_EDGE][x_idx + HALF_ARRAY_EDGE] = true,
+                '#' => cube[0 + HALF_ARRAY_EDGE][0 + HALF_ARRAY_EDGE][y_idx + HALF_ARRAY_EDGE][x_idx + HALF_ARRAY_EDGE] = true,
                 '.' => (),
                 _ => panic!("something went wrong!")
             }
@@ -37,16 +37,17 @@ fn make_cube(boxed_iterator: Box<dyn Iterator< Item = String >>) -> [[[bool; ARR
     cube
 }
 
-fn count_all(cube: &[[[bool; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE]) -> u64 {
+fn count_all(cube: &[[[[bool; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE]) -> u64 {
     (0usize..ARRAY_EDGE as usize)
         .cartesian_product(0usize..ARRAY_EDGE as usize)
         .cartesian_product(0usize..ARRAY_EDGE as usize)
+        .cartesian_product(0usize..ARRAY_EDGE as usize)
         .map(
-            |((z_coord, y_coord), x_coord)| {
-                match cube[z_coord][y_coord][x_coord] {
+            |(((w_coord, z_coord), y_coord), x_coord)| {
+                match cube[w_coord][z_coord][y_coord][x_coord] {
                     true => {
-                        if (z_coord == 0 || y_coord == 0 || x_coord == 0) {panic!("you're gonna need a bigger box")}
-                        if (z_coord >= ARRAY_EDGE || y_coord >= ARRAY_EDGE || x_coord >= ARRAY_EDGE) {panic!("you're gonna need a bigger box")}
+                        if (w_coord == 0 || z_coord == 0 || y_coord == 0 || x_coord == 0) {panic!("you're gonna need a bigger box")}
+                        if (w_coord >= ARRAY_EDGE || z_coord >= ARRAY_EDGE || y_coord >= ARRAY_EDGE || x_coord >= ARRAY_EDGE) {panic!("you're gonna need a bigger box")}
                         1
                     }
                     false => 0
@@ -56,7 +57,8 @@ fn count_all(cube: &[[[bool; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE]) -> u64 {
 }
 
 fn count_neighbors(
-    cube: &[[[bool; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE],
+    cube: &[[[[bool; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE],
+    w_arr: usize,
     z_arr: usize,
     y_arr: usize,
     x_arr: usize
@@ -64,30 +66,34 @@ fn count_neighbors(
     let count = (-1i32..2)
         .cartesian_product(-1i32..2)
         .cartesian_product(-1i32..2)
-        .filter(|((dz, dy), dx)| { 
-            (*dz != 0) || (*dy != 0) || (*dx != 0)
+        .cartesian_product(-1i32..2)
+        .filter(|(((dw, dz), dy), dx)| { 
+            (*dw != 0) || (*dz != 0) || (*dy != 0) || (*dx != 0)
         })
-        .map(|((dz, dy), dx)| { 
+        .map(|(((dw, dz), dy), dx)| { 
             (
+                (w_arr as i32 + dw),
                 (z_arr as i32 + dz),
                 (y_arr as i32 + dy),
                 (x_arr as i32 + dx),
             )
         })
-        .filter(|(z_arr, y_arr, x_arr)| {
-            (*z_arr >= 0 && *z_arr < ARRAY_EDGE as i32)
+        .filter(|(w_arr, z_arr, y_arr, x_arr)| {
+            (*w_arr >= 0 && *w_arr < ARRAY_EDGE as i32)
+            && (*z_arr >= 0 && *z_arr < ARRAY_EDGE as i32)
             && (*y_arr >= 0 && *y_arr < ARRAY_EDGE as i32)
             && (*x_arr >= 0 && *x_arr < ARRAY_EDGE as i32)
         })
-        .map(|(z_arr, y_arr, x_arr)| {
+        .map(|(w_arr, z_arr, y_arr, x_arr)| {
             (
+                w_arr as usize,
                 z_arr as usize,
                 y_arr as usize,
                 x_arr as usize
             )
         })
-        .map (|(z_arr, y_arr, x_arr)| {
-            match cube[z_arr][y_arr][x_arr] {
+        .map (|(w_arr, z_arr, y_arr, x_arr)| {
+            match cube[w_arr][z_arr][y_arr][x_arr] {
                 true => 1,
                 false => 0
             }
@@ -98,23 +104,24 @@ fn count_neighbors(
 }
 
 
-fn step(mut cube: &mut [[[bool; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE]) {
-    let mut next = [[[false; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE];
+fn step(mut cube: &mut [[[[bool; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE]) {
+    let mut next = [[[[false; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE];
     let _: () = (0usize..ARRAY_EDGE as usize)
         .cartesian_product(0usize..ARRAY_EDGE as usize)
         .cartesian_product(0usize..ARRAY_EDGE as usize)
+        .cartesian_product(0usize..ARRAY_EDGE as usize)
         .map(
-            |((z_coord, y_coord), x_coord)| {
-                match cube[z_coord][y_coord][x_coord] {
+            |(((w_coord, z_coord), y_coord), x_coord)| {
+                match cube[w_coord][z_coord][y_coord][x_coord] {
                     true => {
-                        if (count_neighbors(&cube, z_coord, y_coord, x_coord) == 2
-                            || count_neighbors(&cube, z_coord, y_coord, x_coord) == 3) {
-                                next[z_coord][y_coord][x_coord] = true
+                        if (count_neighbors(&cube, w_coord, z_coord, y_coord, x_coord) == 2
+                            || count_neighbors(&cube, w_coord, z_coord, y_coord, x_coord) == 3) {
+                                next[w_coord][z_coord][y_coord][x_coord] = true
                             }
                     }
                     false => {
-                        if count_neighbors(&cube, z_coord, y_coord, x_coord) == 3 {
-                            next[z_coord][y_coord][x_coord] = true
+                        if count_neighbors(&cube, w_coord, z_coord, y_coord, x_coord) == 3 {
+                            next[w_coord][z_coord][y_coord][x_coord] = true
                         }
                     }
                 }
@@ -125,19 +132,6 @@ fn step(mut cube: &mut [[[bool; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE]) {
 
 fn open_file(file_path: &str) -> File {
     File::open(file_path).unwrap()
-}
-
-
-fn print_plane(cube: &[[[bool; ARRAY_EDGE]; ARRAY_EDGE]; ARRAY_EDGE], z_coord: usize) {
-    for y_idx in 0usize..27 {
-        let row: String = (24..ARRAY_EDGE).map(|x_idx| {
-            match cube[z_coord][y_idx][x_idx] {
-                true => '#',
-                false => '.'
-            }
-        }).collect();
-        println!("{}", row);
-    }
 }
 
 
@@ -184,11 +178,11 @@ mod tests {
             456
         */
 
-        let aoc_reader = AocBufReader::from_file(open_file("../data/test_pt1_112.txt"));
-        let mut cube = make_cube(Box::new(aoc_reader));
+        // let aoc_reader = AocBufReader::from_file(open_file("../data/test_pt1_112.txt"));
+        // let mut cube = make_cube(Box::new(aoc_reader));
 
-        assert_eq!(count_neighbors(&cube, 24, 26, 24), 1);
-        assert_eq!(count_neighbors(&cube, 24, 26, 25), 3);
+        // assert_eq!(count_neighbors(&cube, 24, 26, 24), 1);
+        // assert_eq!(count_neighbors(&cube, 24, 26, 25), 3);
     }
 
 }
